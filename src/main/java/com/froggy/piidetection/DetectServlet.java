6535d0a57d1853f8744aa593f4fdd846d16d8673
신성현 <froggy0m0a@gmail.com>
2025-01-31 23:27:49 +0900

    chore: Framework 버전 업그레이드 v0.1.0

--- src/main/java/com/froggy/piidetection/DetectServlet.java
+++ src/main/java/com/froggy/piidetection/DetectServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller(url="/detect")
-public class DetectServlet {
+public class DefaultController {
 
     @RequestMapping(urlPattern = "")
     public ModelAndView process(HttpServletRequest request, HttpServletResponse response)
